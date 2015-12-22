dad25fd943285767859e673faaea7249181bf737
Jordi Prats <jprats@systemadmin.es>
2015-12-22 11:58:32 +0100

    config topbeat

--- manifests/topbeat.pp
+++ manifests/topbeat.pp
@@ -1,7 +1,28 @@
-class beats::topbeat($period='10') inherits beats::params {
+class beats::topbeat(
+                      $period='60',
+                      $index='topbeat',
+                      $logstashhost=undef,
+                      $elasticsearchhost=undef,
+                      $filepath=undef
+                    ) inherits beats::params {
 
   package { 'topbeat':
     ensure => 'installed',
+  }
+
+  file { '/etc/topbeat/topbeat.yml':
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template("${module_name}/topbeat/topbeat.erb"),
+    require => Package['topbeat'],
+  }
+
+  service { 'topbeat':
+    ensure  => 'running',
+    enable  => true,
+    require => File['/etc/topbeat/topbeat.yml'],
   }
 
 }
