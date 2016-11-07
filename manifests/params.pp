d14f9159548607ff97532ebc5d98cc38c443baaa
Jordi Prats <jprats@systemadmin.es>
2016-11-07 13:57:48 +0100

    lint

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,50 +1,50 @@
 class beats::params {
 
-	case $::osfamily
-	{
-		'redhat':
+  case $::osfamily
+  {
+    'redhat':
     {
-			$filebeat_paths_default = [
-																	'/var/log/messages',
-																	'/var/log/secure',
-																	'/var/log/yum.log',
-																	'/var/log/cron',
-																	'/var/log/maillog',
-																]
-			case $::operatingsystemrelease
-			{
-				/^[67].*$/:
-				{
-					$yumrepo=true
-				}
-				default: { fail("Unsupported RHEL/CentOS version!")  }
-			}
-		}
-		'Debian':
-		{
-			$filebeat_paths_default = [
-																	'/var/log/auth.log',
-																	'/var/log/syslog',
-																	'/var/log/dpkg.log',
-																	'/var/log/mail.log',
-																]
-			case $::operatingsystem
-			{
-				'Ubuntu':
-				{
-					case $::operatingsystemrelease
-					{
-						/^14.*$/:
-						{
-							$yumrepo=false
-						}
-						default: { fail("Unsupported Ubuntu version! - $::operatingsystemrelease")  }
-					}
-				}
-				'Debian': { fail("Unsupported")  }
-				default: { fail("Unsupported Debian flavour!")  }
-			}
-		}
-		default: { fail("Unsupported OS!")  }
-	}
+      $filebeat_paths_default = [
+                                  '/var/log/messages',
+                                  '/var/log/secure',
+                                  '/var/log/yum.log',
+                                  '/var/log/cron',
+                                  '/var/log/maillog',
+                                ]
+      case $::operatingsystemrelease
+      {
+        /^[67].*$/:
+        {
+          $yumrepo=true
+        }
+        default: { fail("Unsupported RHEL/CentOS version!")  }
+      }
+    }
+    'Debian':
+    {
+      $filebeat_paths_default = [
+                                  '/var/log/auth.log',
+                                  '/var/log/syslog',
+                                  '/var/log/dpkg.log',
+                                  '/var/log/mail.log',
+                                ]
+      case $::operatingsystem
+      {
+        'Ubuntu':
+        {
+          case $::operatingsystemrelease
+          {
+            /^14.*$/:
+            {
+              $yumrepo=false
+            }
+            default: { fail("Unsupported Ubuntu version! - $::operatingsystemrelease")  }
+          }
+        }
+        'Debian': { fail("Unsupported")  }
+        default: { fail("Unsupported Debian flavour!")  }
+      }
+    }
+    default: { fail("Unsupported OS!")  }
+  }
 }
