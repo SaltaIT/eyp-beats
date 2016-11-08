e39e7726732896fbd8e35eeb9574fbbb25d39f91
Jordi Prats <jprats@systemadmin.es>
2016-11-08 16:32:35 +0100

    canvi a concat

--- manifests/filebeat.pp
+++ manifests/filebeat.pp
@@ -12,19 +12,42 @@ class beats::filebeat (
     require => Class['beats'],
   }
 
-  file { '/etc/filebeat/filebeat.yml':
+  concat { '/etc/filebeat/filebeat.yml':
     ensure  => 'present',
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
-    content => template("${module_name}/filebeat/filebeat.erb"),
     require => Package['filebeat'],
+  }
+
+  concat::fragment { 'filebeat header config':
+    target  => '/etc/filebeat/filebeat.yml',
+    order   => '00',
+    content => template("${module_name}/filebeat/filebeat.erb"),
+  }
+
+  concat::fragment { 'filebeat output config':
+    target  => '/etc/filebeat/filebeat.yml',
+    order   => '10',
+    content => template("${module_name}/output.erb"),
+  }
+
+  concat::fragment { 'filebeat shipper config':
+    target  => '/etc/filebeat/filebeat.yml',
+    order   => '20',
+    content => template("${module_name}/shipper.erb"),
+  }
+
+  concat::fragment { 'filebeat logging config':
+    target  => '/etc/filebeat/filebeat.yml',
+    order   => '30',
+    content => template("${module_name}/logging.erb"),
   }
 
   service { 'filebeat':
     ensure  => 'running',
     enable  => true,
-    require => File['/etc/filebeat/filebeat.yml'],
+    require => Concat['/etc/filebeat/filebeat.yml'],
   }
 
 }
--- manifests/topbeat.pp
+++ manifests/topbeat.pp
@@ -9,19 +9,42 @@ class beats::topbeat(
     require => Class['beats'],
   }
 
-  file { '/etc/topbeat/topbeat.yml':
+  concat { '/etc/topbeat/topbeat.yml':
     ensure  => 'present',
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
-    content => template("${module_name}/topbeat/topbeat.erb"),
     require => Package['topbeat'],
+  }
+
+  concat::fragment { 'topbeat header config':
+    target  => '/etc/topbeat/topbeat.yml',
+    order   => '00',
+    content => template("${module_name}/topbeat/topbeat.erb"),
+  }
+
+  concat::fragment { 'topbeat output config':
+    target  => '/etc/topbeat/topbeat.yml',
+    order   => '10',
+    content => template("${module_name}/output.erb"),
+  }
+
+  concat::fragment { 'topbeat shipper config':
+    target  => '/etc/topbeat/topbeat.yml',
+    order   => '20',
+    content => template("${module_name}/shipper.erb"),
+  }
+
+  concat::fragment { 'topbeat logging config':
+    target  => '/etc/topbeat/topbeat.yml',
+    order   => '30',
+    content => template("${module_name}/logging.erb"),
   }
 
   service { 'topbeat':
     ensure  => 'running',
     enable  => true,
-    require => File['/etc/topbeat/topbeat.yml'],
+    require => Concat['/etc/topbeat/topbeat.yml'],
   }
 
 }
