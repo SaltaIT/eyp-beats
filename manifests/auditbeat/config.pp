d7755a58c85753e74f96b07985e82985bd9cc30b
Jordi Prats <jordi.prats@gmail.com>
2018-10-23 12:31:13 +0200

    lint

--- manifests/auditbeat/config.pp
+++ manifests/auditbeat/config.pp
@@ -1,10 +1,10 @@
 class beats::auditbeat::config inherits beats::auditbeat {
 
   file { '/etc/auditbeat/auditbeat.yml':
-    ensure => 'present',
-    owner => 'root',
-    group => 'root',
-    mode => '0600',
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0600',
     content => template("${module_name}/auditbeat/config.erb"),
   }
 }
