441e10eca26dc396a14f572527c3cbeae7cb7b55
Jordi Prats <jordi.prats@gmail.com>
2018-10-24 16:43:17 +0200

    test fsrule

--- examples/auditbeat.pp
+++ examples/auditbeat.pp
@@ -4,8 +4,13 @@ class { 'beats::auditbeat':
 
 # -a always,exit -F arch=b64 -S execve,execveat -k exec
 beats::auditbeat::syscallrule { 'execve':
-  actions => [ 'always', 'exit' ],
+  actions  => [ 'always', 'exit' ],
   syscalls => [ 'execve' ],
+}
+
+beats::auditbeat::fsrule { 'exec_tmp':
+  path        => '/tmp',
+  permissions => 'x',
 }
 
 class { 'beats::filebeat':
@@ -13,7 +18,7 @@ class { 'beats::filebeat':
 }
 
 beats::filebeat::input { 'auditbeat':
-  paths =>  [ '/var/log/audit/auditbeat' ],
+  paths                => [ '/var/log/audit/auditbeat' ],
+  fields               => { 'auditbeat' => 'true' },
   json_keys_under_root => true,
-  fields => { 'auditbeat' => 'true' },
 }
