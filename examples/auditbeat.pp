ad74c240c70f80fa4a378f4e5bb3f7cbeb7caeb7
Jordi Prats <jordi.prats@gmail.com>
2018-10-24 11:32:47 +0200

    demo beats

--- examples/auditbeat.pp
+++ examples/auditbeat.pp
@@ -7,3 +7,13 @@ beats::auditbeat::syscallrule { 'execve':
   actions => [ 'always', 'exit' ],
   syscalls => [ 'execve' ],
 }
+
+class { 'beats::filebeat':
+  logstash_hosts => [ '127.0.0.1:5044'],
+}
+
+beats::filebeat::input { 'auditbeat':
+  paths =>  [ '/var/log/audit' ],
+  json_keys_under_root => true,
+  fields => { 'auditbeat' => 'true' },
+}
