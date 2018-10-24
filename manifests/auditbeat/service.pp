784853c36279130dd0b8f018acda119da903ff41
Jordi Prats <jordi.prats@gmail.com>
2018-10-24 17:21:33 +0200

    neteja validates

--- manifests/auditbeat/service.pp
+++ manifests/auditbeat/service.pp
@@ -1,10 +1,4 @@
 class beats::auditbeat::service inherits beats::auditbeat {
-  #
-  validate_bool($beats::auditbeat::manage_docker_service)
-  validate_bool($beats::auditbeat::manage_service)
-  validate_bool($beats::auditbeat::service_enable)
-
-  validate_re($beats::auditbeat::service_ensure, [ '^running$', '^stopped$' ], "Not a valid daemon status: ${beats::auditbeat::service_ensure}")
 
   $is_docker_container_var=getvar('::eyp_docker_iscontainer')
   $is_docker_container=str2bool($is_docker_container_var)
--- manifests/filebeat/service.pp
+++ manifests/filebeat/service.pp
@@ -1,10 +1,4 @@
 class beats::filebeat::service inherits beats::filebeat {
-  #
-  validate_bool($beats::filebeat::manage_docker_service)
-  validate_bool($beats::filebeat::manage_service)
-  validate_bool($beats::filebeat::service_enable)
-
-  validate_re($beats::filebeat::service_ensure, [ '^running$', '^stopped$' ], "Not a valid daemon status: ${beats::filebeat::service_ensure}")
 
   $is_docker_container_var=getvar('::eyp_docker_iscontainer')
   $is_docker_container=str2bool($is_docker_container_var)
